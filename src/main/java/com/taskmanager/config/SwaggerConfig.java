a8a6909a2defefce08255b32f4167738639d399a
wilson1234567891962 <wilson1234567891962@gmail.com>
2026-05-23 17:22:56 -0500

    actualize para correr con jety

--- src/main/java/com/taskmanager/config/SwaggerConfig.java
+++ src/main/java/com/taskmanager/config/SwaggerConfig.java
@@ -1,16 +1,10 @@
 package com.taskmanager.config;
 
-import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.servers.Server;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-import java.util.HashSet;
-import java.util.Set;
 
 @OpenAPIDefinition(
     info = @Info(
@@ -33,16 +27,7 @@ import java.util.Set;
         )
     }
 )
-@ApplicationPath("api")
-public class SwaggerConfig extends Application {
-    
-    @Override
-    public Set<Class<?>> getClasses() {
-        Set<Class<?>> resources = new HashSet<>();
-        
-        // Agregar recursos de Swagger
-        resources.add(OpenApiResource.class);
-        
-        return resources;
-    }
+public class SwaggerConfig {
+    // Esta clase solo contiene anotaciones de OpenAPI
+    // La configuración principal está en ApplicationConfig.java
 }
